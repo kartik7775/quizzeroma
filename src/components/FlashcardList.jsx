b5b757126c8668573b23a121828c79207d62df58
Kartik Nikam <krt1993@outlook.com>
2023-05-18 12:00:33 +0530

    first commit

---
+++ src/components/FlashcardList.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import Flashcard from "./Flashcard";
+
+const FlashcardList = ({ flashcards }) => {
+  return (
+    <div className="card-grid">
+      {flashcards.map((flashcard) => {
+        return <Flashcard flashcard={flashcard} key={flashcard.id} />;
+      })}
+    </div>
+  );
+};
+
+export default FlashcardList;
